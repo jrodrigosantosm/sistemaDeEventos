d6deafbb6982e6a794e9800b9d46a1abe68e58ec
Rodrigo Santos <rodrigom376@gmail.com>
2024-02-17 21:18:31 -0300

    consulta graduaçoes

--- backend/controllers/UsuarioController.php
+++ backend/controllers/UsuarioController.php
@@ -3,135 +3,12 @@
 namespace app\controllers;
 
 use app\models\ArtesMarciais;
-use app\models\Usuarios;
-use Yii;
-use yii\data\ActiveDataProvider;
+use app\models\Graduacoes;
 use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
 
-/**
- * UsuarioController implements the CRUD actions for Usuarios model.
- */
+
 class UsuarioController extends Controller
 {
-    /**
-     * @inheritDoc
-     */
-    public function behaviors()
-    {
-        return array_merge(
-            parent::behaviors(),
-            [
-                'verbs' => [
-                    'class' => VerbFilter::className(),
-                    'actions' => [
-                        'delete' => ['POST'],
-                    ],
-                ],
-            ]
-        );
-    }
-
-    /**
-     * Lists all Usuarios models.
-     *
-     * @return string
-     */
-    public function actionIndex()
-    {
-        $usuarios = Usuarios::find()->all();
-        return $this->render('index', [
-            'usuarios' => $usuarios,
-        ]);
-    }
-
-
-    /**
-     * Displays a single Usuarios model.
-     * @param int $id ID
-     * @return string
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
-     * Creates a new Usuarios model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return string|\yii\web\Response
-     */
-    public function actionCreate()
-    {
-        $model = new Usuarios();
-
-        if ($this->request->isPost) {
-            if ($model->load($this->request->post()) && $model->save()) {
-                return $this->redirect(['view', 'id' => $model->id]);
-            }
-        } else {
-            $model->loadDefaultValues();
-        }
-
-        return $this->render('create', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Updates an existing Usuarios model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param int $id ID
-     * @return string|\yii\web\Response
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Deletes an existing Usuarios model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param int $id ID
-     * @return \yii\web\Response
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * Finds the Usuarios model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param int $id ID
-     * @return Usuarios the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = Usuarios::findOne(['id' => $id])) !== null) {
-            return $model;
-        }
-
-        throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
-    }
-
     public function actionGetArtesMarciais()
     {
         $artesMarciais = ArtesMarciais::find()
@@ -139,5 +16,12 @@ class UsuarioController extends Controller
             ->all();
 
         return $artesMarciais;
+    }
+
+    public function actionGetGraduacao($id)
+    {
+        $graduacoes = Graduacoes::findAll(['id_arte_marcial' => $id]);
+
+        return $graduacoes;
     }
 }
