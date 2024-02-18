61f002dfe5915991f83ac1aaeb0b8e7392296ec7
Rodrigo Santos <rodrigom376@gmail.com>
2024-02-18 20:46:58 -0300

    modal home para loguin e cadastro de novos usuarios

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './componentes/home/home.component';
+import { ModalHomeComponent } from './componentes/home/modal-home/modal-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ModalHomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/componentes/home/modal-home/modal-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-home',
+  templateUrl: './modal-home.component.html',
+  styleUrl: './modal-home.component.scss'
+})
+export class ModalHomeComponent {
+
+}
